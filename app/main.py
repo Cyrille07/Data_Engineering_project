357e81d5a8229bbeb2f499cd84d1e6a0d7da2e7d
Cyrille07 <bio.kouma@edu.esiee.fr>
2025-02-11 16:07:19 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -33,4 +33,4 @@ def display_page(pathname):
         return home.HomePage()  # Page d'accueil par défaut
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(host="0.0.0.0", port=8050, debug=True)
